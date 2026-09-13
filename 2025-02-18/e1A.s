0a177a198ddadccb6f73e875b0edce27295da7d0
Nicolò Capoccetti <capoccettinicolo7@gmail.com>
2026-09-13 16:15:32 +0200

    Add files via upload

---
+++ 2025-02-18/e1A.s
@@ -0,0 +1,41 @@
+.globl _count_tokens
+
+_count_tokens:
+    #PROLOGO
+    pushl %ebx
+    pushl %esi
+    pushl %edi
+    pushl %ebp
+    subl $8,%esp
+
+    movl 28(%esp),%ebx
+    movl 32(%esp),%esi
+    xorl %edi,%edi
+
+    movl %ebx,(%esp)
+    movl %esi,4(%esp)
+    call _strtok
+    movl %eax,%ebp
+
+    E:
+        cmpl $0,%ebp
+        je F
+
+        incl %edi
+
+        movl $0,(%esp)
+        movl %esi,4(%esp)
+        call _strtok
+        movl %eax,%ebp
+
+        jmp E
+    F:
+        movl %edi,%eax
+    #EPILOGO
+        addl $8,%esp
+        popl %ebp
+        popl %edi
+        popl %esi
+        popl %ebx
+
+        ret
